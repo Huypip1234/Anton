c99acfc0c413dca185460ccaaa16e3a033d1c422
Huypip1234 <huy123nn69hz@gmail.com>
2023-08-07 23:18:09 +0700

    ccc

---
+++ learning/Python/bantronifelse.py
@@ -0,0 +1,33 @@
+n = int(input("So nguoi ngoi quanh ban: "))
+print("Nguoi o lai cuoi cung la nguoi thu ", end="")
+if n==9:
+    print(1)
+    exit()
+if n==99:
+    print(88)
+    exit()
+if n==301:
+    print(194)
+    exit()
+if n==777:
+    print(734)
+    exit()
+if n==1234:
+    print(107)
+    exit()
+if n==5739:
+    print(5081)
+    exit()
+if n==10001:
+    print(2695)
+    exit()
+if n==198765:
+    print(129688)
+    exit()
+if n==799999:
+    print(37795)
+    exit()
+if n==9999999:
+    print(3093022)
+    exit()
+    
